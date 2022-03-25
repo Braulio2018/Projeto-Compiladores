7f783c926c662117cfc6e0bf1c0c0a3042e59377
Daniel Augusto Veronezi Salvador <dvsalvador.ds@gmail.com>
2022-03-25 02:40:12 -0300

    Create Main

--- src/main/java/com/gutoveronezi/compiler/Main.java
+++ src/main/java/com/gutoveronezi/compiler/Main.java
@@ -3,11 +3,11 @@ package com.gutoveronezi.compiler;
 import com.gutoveronezi.compiler.views.EditorView;
 import javax.swing.JFrame;
 
-public class Compiler {
+public class Main {
 
     public static void main(String[] args) {
         EditorView view = new EditorView();
         view.setVisible(true);
         view.setExtendedState(JFrame.MAXIMIZED_BOTH);
-    }
+    }    
 }
